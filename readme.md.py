dd361e861ff32bcf66a54a2e73b127770e427593
TariqKarimi <84006790+TariqKarimi@users.noreply.github.com>
2021-05-18 20:19:07 -0700

    Update Python code
    Hello Sarmad Kubba! Please find and fix the bug if necessary

--- readme.md.py
+++ readme.md.py
@@ -1,12 +1,5 @@
-def sumTo():
-    """ Return the sum of reciprocals of powers of 2 """
-
-    theSum  = 0
-    aNumber = 0
-    while theSum < 2.0:
-        theSum = theSum + 1/2**aNumber
-        aNumber = aNumber + 1
-
-    return theSum
-
-print(sumTo())
+def lone_sum(a, b, c):    if a >= b:        
+        return c    elif a == c:        
+        return b    elif b == c:        
+        return a    elif a == b and a == c and b == c:        
+        return 0    else:        return a+b+c
